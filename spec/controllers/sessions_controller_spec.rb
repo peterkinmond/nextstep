61c2ffb4ecedfb45110010880136b871bf8f2c33
Peter Kinmond <peterkinmond@gmail.com>
2011-09-04 18:44:18 -0700

    change link after signin to projects page

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -47,9 +47,9 @@ describe SessionsController do
            controller.should be_signed_in
         end
         
-        it "should redirect to the user show page" do
+        it "should redirect to the projects index page" do
            post :create, :session => @attr
-           response.should redirect_to(user_path(@user))
+           response.should redirect_to(projects_path)
         end
      end
   end
