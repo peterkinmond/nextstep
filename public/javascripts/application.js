5bfec7a4110974e4b8deb9a777319e3963847d4b
Peter Kinmond <peterkinmond@gmail.com>
2011-09-05 22:40:04 -0700

    add watermark to "Add Project" and "Add Step" text boxes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,47 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+                                            
+// Script by Drew Noakes
+// http://www.drewnoakes.com/code/javascript/hintTextbox.html
+// 14 Dec 2006 - Initial release
+// 08 Jun 2010 - Added support for password textboxes
+
+var HintClass = "hintTextbox";
+var HintActiveClass = "hintTextboxActive";
+
+// define a custom method on the string class to trim leading and training spaces
+String.prototype.trim = function() { return this.replace(/^\s+|\s+$/g, ''); };
+
+function initHintTextboxes() {
+  var inputs = document.getElementsByTagName('input');
+  for (i=0; i<inputs.length; i++) {
+    var input = inputs[i];
+    if (input.type!="text" && input.type!="password")
+      continue;
+      
+    if (input.className.indexOf(HintClass)!=-1) {
+      input.hintText = input.value;
+      input.className = HintClass;
+      input.onfocus = onHintTextboxFocus;
+      input.onblur = onHintTextboxBlur;
+    }
+  }
+}
+
+function onHintTextboxFocus() {
+  var input = this;
+  if (input.value.trim()==input.hintText) {
+    input.value = "";
+    input.className = HintActiveClass;
+  }
+}
+
+function onHintTextboxBlur() {
+  var input = this;
+  if (input.value.trim().length==0) {
+    input.value = input.hintText;
+    input.className = HintClass;
+  }
+}
+
+window.onload = initHintTextboxes;
