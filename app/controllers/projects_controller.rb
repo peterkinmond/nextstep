ead8e59b33ee2d463b45bd20ef98fbc7934e31a1
Peter Kinmond <peterkinmond@gmail.com>
2011-09-05 21:33:54 -0700

    add Projects#New and Projects#Create actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,23 @@
 class ProjectsController < ApplicationController
   before_filter :authenticate
-  
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = current_user.projects.build(params[:project])
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
   def index
     @projects = Project.where(:user_id => current_user)
   end
- 
+
   def show
     @project = Project.find(params[:id])
   end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,69 +2,100 @@ require 'spec_helper'
 
 describe ProjectsController do
   render_views
-  
+
   describe "access control" do
     it "should deny access to 'index'" do
       get :index
       response.should redirect_to(signin_path)
     end
   end
-  
-  describe "GET 'index'" do 
-    
-    describe "user associations" do
-    
+
+  describe "GET 'new'" do
+    before(:each) do
+      @user = test_sign_in(Factory(:user))
+    end 
+
+    it "should be successful" do
+      get :new
+      response.should be_success
+    end
+  end
+
+  describe "POST 'create'" do
+    describe "success" do
       before(:each) do
-         @user = test_sign_in(Factory(:user))
-         @project = @user.projects.create(:name => "User 1's Project")
-         
-         @other_user = Factory(:user, :email => Factory.next(:email)) 
-         @other_user_project = @other_user.projects.create(:name => "Other user's project")
+        @user = test_sign_in(Factory(:user))  
+        @project = @user.projects.create(:name => "New Project")
       end
-    
-       it "should be successful" do
-          get :index
-          response.should be_success
-       end 
-       
-       it "should show projects that belong to current user" do
-         get :index      
-         response.should have_selector('td.projectname', :content => "User 1's Project")
-       end
-             
-       it "should NOT show projects that DO NOT belong to current user" do
-          get :index
-          response.should_not have_selector('td.projectname', :content => "Other user's project")
-       end
-       
-       it "should link to project" do
-          get :index
-          response.should have_selector('a',  :href => project_path(@project),
-                                              :content => "User 1's Project")
-       end
-       
-       it "should show the next step for each project" do
-          get :index
-          response.should have_selector('td.nextstep', :content => "None")
-       end
-   end
+
+      it "should create a project" do
+        lambda do
+          post :create, :project  => @project
+        end.should change(Project, :count).by(1)
+      end
+      
+      it "should redirect to the project index page" do
+        post :create, :project => @project
+        response.should redirect_to(projects_path)
+      end 
+    end
+  end
+
+  describe "GET 'index'" do 
+
+    describe "user associations" do
+
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        @project = @user.projects.create(:name => "User 1's Project")
+
+        @other_user = Factory(:user, :email => Factory.next(:email)) 
+        @other_user_project = @other_user.projects.create(:name => "Other user's project")
+      end
+
+      it "should be successful" do
+        get :index
+        response.should be_success
+      end 
+
+      it "should show projects that belong to current user" do
+        get :index      
+        response.should have_selector('td.projectname', :content => "User 1's Project")
+      end
+
+      it "should NOT show projects that DO NOT belong to current user" do
+        get :index
+        response.should_not have_selector('td.projectname', :content => "Other user's project")
+      end
+
+      it "should link to project" do
+        get :index
+        response.should have_selector('a',  :href => project_path(@project),
+        :content => "User 1's Project")
+      end
+
+      it "should show the next step for each project" do
+        get :index
+        response.should have_selector('td.nextstep', :content => "None")
+      end
+    end
   end
 
   describe "GET 'show'" do
-    
+
     before(:each) do
-       @user = test_sign_in(Factory(:user))
-       @project = @user.projects.create(:name => "La Project Du Pierre")
+      @user = test_sign_in(Factory(:user))
+      @project = @user.projects.create(:name => "La Project Du Pierre")
     end
-    
-     it "should be successful" do
-        get :show, :id => @project
-        response.should be_success
-     end
-     
-     it "should have the right project" do
-        get :show, :id => @project
-        assigns(:project).should == @project
-     end
+
+    it "should be successful" do
+      get :show, :id => @project
+      response.should be_success
+    end
+
+    it "should have the right project" do
+      get :show, :id => @project
+      assigns(:project).should == @project
+    end
   end
 end
