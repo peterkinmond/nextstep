83cf79c978b9dc49c08cfbe424bad3a7efb56ae0
Peter Kinmond <peterkinmond@gmail.com>
2011-09-09 09:08:39 -0700

    add jquery.hotkeys.js in order to add keyboard shortcuts

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,4 +4,21 @@
 // Make checkboxes auto-update form
 $('.submittable').live('change', function() {
   $(this).parents('form:first').submit();
-});
+});   
+
+
+// Add hot keys! So hot!!
+function domo(){
+    jQuery('#platform-details').html('<code>' + navigator.userAgent + '</code>');
+    jQuery(document).bind('keydown', '/', function (evt){
+        alert("Hello Slash"); 
+        return false; 
+    });
+    jQuery(document).bind('keydown', 'ctrl+p meta+p', function (evt){
+        alert("think green-don't print"); 
+        return false; 
+    });
+}
+
+
+jQuery(document).ready(domo);     
