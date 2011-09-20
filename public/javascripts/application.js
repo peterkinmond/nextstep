2e14da3fb80bdd09e83b327430f07bc1dd059bea
Wayne Mak <wayne@sharethrough.com>
2011-09-19 22:51:01 -0700

    revert text areas to text fields; add keyboard shortcuts

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,30 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-                                            
+
 // Make checkboxes auto-update form
 $('.submittable').live('change', function() {
-  $(this).parents('form:first').submit();
-});   
+    $(this).parents('form:first').submit();
+});
 
 
 // Add hot keys! So hot!!
-function domo(){
-    jQuery('#platform-details').html('<code>' + navigator.userAgent + '</code>');
-    jQuery(document).bind('keydown', '/', function (evt){
-        alert("Hello Slash"); 
-        return false; 
+function domo() {
+    // Add new project
+    jQuery(document).bind('keydown', 'p', function (evt) {
+        $('#project_name').focus();
+        return false;
     });
-    jQuery(document).bind('keydown', 'ctrl+p meta+p', function (evt){
-        alert("think green-don't print"); 
-        return false; 
+
+    // Add new step
+    jQuery(document).bind('keydown', 's', function (evt) {
+        $('#step_content').focus();
+        return false;
+    });
+
+    // Select project#index page
+    jQuery(document).bind('keydown', 'shift+p', function (evt) {
+        $(location).attr('href', '/projects');
+        return false;
     });
 }
 
