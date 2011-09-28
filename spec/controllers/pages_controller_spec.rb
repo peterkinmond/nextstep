e0a34fbbbd0da9301a032b386e967c6764b5bf6d
Peter Kinmond <peterkinmond@gmail.com>
2011-09-27 21:52:06 -0700

    Added title to About and Contact pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe PagesController do
 
-  describe "GET 'home'" do
+  describe "GET 'about'" do
     it "should be successful" do
-      get 'home'
+      get 'about'
       response.should be_success
     end
   end
