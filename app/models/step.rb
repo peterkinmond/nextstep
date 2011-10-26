bffd66f8a1fbd9cde16f61a146667d66e96f7f34
Peter Kinmond <peterkinmond@gmail.com>
2011-10-25 22:00:33 -0700

    Set initial position of step to Step.id

--- app/models/step.rb
+++ app/models/step.rb
@@ -12,10 +12,17 @@
 #
 
 class Step < ActiveRecord::Base
-  attr_accessible :content, :estimated_time, :completed, :project_id, :urgent, :important
+  attr_accessible :content, :estimated_time, :completed, :urgent, :important, :position
   
   belongs_to :project
   
   validates :content,     :presence => true
   validates :project,  :presence => true
+  
+  after_create  :set_position
+  
+  def set_position
+     self.position = self.id
+     self.save
+  end
 end
--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -2,10 +2,19 @@ require 'spec_helper'
 
 describe Step do
   it { should belong_to(:project) }
+
   it { should validate_presence_of(:content)}
   it { should validate_presence_of(:project)}
+
   it { should have_db_column(:completed).of_type(:boolean)}  
   it { should have_db_column(:urgent).of_type(:boolean)} 
   it { should have_db_column(:important).of_type(:boolean)} 
   it { should have_db_column(:position).of_type(:integer)}
+
+  it { should allow_mass_assignment_of(:content)}
+  it { should allow_mass_assignment_of(:estimated_time)}
+  it { should allow_mass_assignment_of(:completed)}
+  it { should allow_mass_assignment_of(:urgent)}
+  it { should allow_mass_assignment_of(:important)}
+  it { should allow_mass_assignment_of(:position)}
 end                                    
