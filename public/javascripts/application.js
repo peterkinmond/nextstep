786d464654c488cc42d644e3217b80ee61466326
Peter Kinmond <engineers+peter@sharethrough.com>
2012-01-10 21:35:52 -0800

    Make js shortcut for getting to project pages dynamic and less horrible.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,86 +3,68 @@
 
 // Make checkboxes auto-update form
 $('.submittable').live('change', function() {
-	$(this).parents('form:first').submit();
+    $(this).parents('form:first').submit();
 });
 
 
 // Add hot keys! So hot!!
 function domo() {
-	// Add new project
-    if(window.location.pathname === '/projects') {
+    // Add new project
+    if (window.location.pathname === '/projects') {
         jQuery(document).bind('keydown', 'p', function (evt) {
             $('#project_name').focus();
             return false;
         });
     }
 
-	// Add new step
-    if(window.location.pathname.indexOf("/steps") != -1) {
+    // Add new step
+    if (window.location.pathname.indexOf("/steps") != -1) {
         jQuery(document).bind('keydown', 's', function (evt) {
             $('#step_content').focus();
             return false;
         });
     }
 
-	// Select project#index page
-	jQuery(document).bind('keydown', 'shift+p', function (evt) {
-		$(location).attr('href', '/projects');
-		return false;
-	});
+    // Select project#index page
+    jQuery(document).bind('keydown', 'shift+p', function (evt) {
+        $(location).attr('href', '/projects');
+        return false;
+    });
 
-	// Select todo page
-	jQuery(document).bind('keydown', 'shift+t', function (evt) {
-		$(location).attr('href', '/todo');
-		return false;
-	});
+    // Select todo page
+    jQuery(document).bind('keydown', 'shift+t', function (evt) {
+        $(location).attr('href', '/todo');
+        return false;
+    });
 
-
-	// Select specific project's page
-
-	// TODO: Clean up ugly copy paste
-	if(window.location.pathname === '/projects') {
-		jQuery(document).bind('keydown', 'shift+1', function (evt) {
-			$(location).attr('href', '/projects/' + $('#1').attr('project_id') + '/steps');
-			return false;
-		});
-
-		jQuery(document).bind('keydown', 'shift+2', function (evt) {
-			$(location).attr('href', '/projects/' + $('#2').attr('project_id') + '/steps');
-			return false;
-		});
-
-		jQuery(document).bind('keydown', 'shift+3', function (evt) {
-			$(location).attr('href', '/projects/' + $('#3').attr('project_id') + '/steps');
-			return false;
-		});
-
-		jQuery(document).bind('keydown', 'shift+4', function (evt) {
-			$(location).attr('href', '/projects/' + $('#4').attr('project_id') + '/steps');
-			return false;
-		});
-
-		jQuery(document).bind('keydown', 'shift+5', function (evt) {
-			$(location).attr('href', '/projects/' + $('#5').attr('project_id') + '/steps');
-			return false;
-		});
-	}
+    // Select specific project's page('shift + 1' takes you to first listed project, etc)
+    if (window.location.pathname === '/projects') {
+        for (i = 1; i <= 9; i++) {
+            (function(i) {
+                jQuery(document).bind('keydown', 'shift+' + i, function (evt) {
+                    if ($('#' + i).attr('project_id')) {
+                        $(location).attr('href', '/projects/' + $('#' + i).attr('project_id') + '/steps');
+                        return false;
+                    }
+                });
+            })(i);
+        };
+    }
 }
 
 jQuery(document).ready(domo);
 
 
-
 // Make the project steps sortable
 $(document).ready(function() {
-	$("#stepTable tbody.content").sortable({
-		update: function(){
-			$.ajax({
-				type: 'post',
-				data: $('#stepTable tbody.content').sortable('serialize'),
-				dataType: 'script',
-				url: 'steps/sort'
-			})
-		}
-	})
+    $("#stepTable tbody.content").sortable({
+        update: function() {
+            $.ajax({
+                type: 'post',
+                data: $('#stepTable tbody.content').sortable('serialize'),
+                dataType: 'script',
+                url: 'steps/sort'
+            })
+        }
+    })
 });
